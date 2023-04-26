e285b43cab986118915d270c0a6df4099de3b4e2
Rory Hackney <72114389+roryhackney@users.noreply.github.com>
2023-04-25 20:15:53 -0700

    WeatherReading Record
    Represents one line of data from weather data file

---
+++ Utility.java
@@ -0,0 +1,56 @@
+/**
+ * Helper methods
+ */
+public class Utility {
+    /**
+     * No instantiating, use static methods
+     */
+    private Utility() {}
+
+    /**
+     * Verifies item is not null or blank and throws appropriate exception message
+     * @param context  context for the item, used as a label, usually the variable name
+     * @param item  the item to be checked
+     */
+    public static void verifyNotNullOrBlank(String context, String item) {
+        if (item == null) {
+            throw new IllegalArgumentException(context + " cannot be null");
+        }
+        if (item.isBlank()) {
+            throw new IllegalArgumentException(context + " cannot be blank");
+        }
+    }
+
+    /**
+     * Verifies integer is in range min through max (inclusive) and throws appropriate exception message
+     * @param  min      minimum value permitted (inclusive)
+     * @param  max      maximum value permitted (inclusive)
+     * @param  context  context for the item, used as a label, usually the variable name
+     * @param  item     the item to be checked
+     */
+    public static void verifyInRange(int min, int max, String context, int item) {
+        if (item < min) {
+            throw new IllegalArgumentException(context + " cannot be less than " + min);
+        }
+        if (item > max) {
+            throw new IllegalArgumentException(context + " cannot be greater than " + max);
+        }
+    }
+
+    /**
+     * Verifies double is in range min through max (inclusive) and throws appropriate exception message
+     * @param  min      minimum value permitted (inclusive)
+     * @param  max      maximum value permitted (inclusive)
+     * @param  context  context for the item, used as a label, usually the variable name
+     * @param  item     the item to be checked
+     */
+    public static void verifyInRange(double min, double max, String context, double item) {
+        if (item - min < 0.000000) {
+            throw new IllegalArgumentException(context + " cannot be less than " + min);
+        }
+        if (item - max < 0.000000) {
+            throw new IllegalArgumentException(context + " cannot be greater than " + max);
+        }
+    }
+
+}
---
+++ WeatherReading.java
@@ -0,0 +1,64 @@
+/**
+ * Represents a single weather reading data line from the city_temperature.csv data file
+ * @param region Continental region the data was taken in; must not be null or blank
+ * @param country Country the data was taken in; must not be null or blank
+ * @param state State the data was taken in (can be blank); must not be null
+ * @param city City the data was taken in; must not be null or blank
+ * @param month Month the data was taken during; must be 1 through 12
+ * @param day Day the data was taken during; must be 1 through 31
+ * @param year Year the data was taken during; must be 1800 through 3000
+ * @param avgTemperature Average temperature measured on this day in this place; must be -99 through 200
+ */
+public record WeatherReading(String region, String country, String state, String city, int month, int day, int year,
+                             double avgTemperature) implements Comparable<WeatherReading> {
+
+    /**
+     * Constructor
+     * @param region Continental region the data was taken in; must not be null or blank
+     * @param country Country the data was taken in; must not be null or blank
+     * @param state State the data was taken in (can be blank); must not be null
+     * @param city City the data was taken in; must not be null or blank
+     * @param month Month the data was taken during; must be 1 through 12
+     * @param day Day the data was taken during; must be 1 through 31
+     * @param year Year the data was taken during; must be 1800 through 3000
+     * @param avgTemperature Average temperature measured on this day in this place; must be -99 through 200
+     */
+    public WeatherReading {
+        Utility.verifyNotNullOrBlank("region", region);
+        Utility.verifyNotNullOrBlank("country", country);
+        if (state == null) {throw new IllegalArgumentException("state cannot be null");}
+        Utility.verifyNotNullOrBlank("city", city);
+        Utility.verifyInRange(1, 12, "month", month);
+        Utility.verifyInRange(1, 31, "day", day);
+        Utility.verifyInRange(1800, 3000, "year", year);
+        Utility.verifyInRange(-99.0, 200.0, "avgTemperature", avgTemperature);
+
+
+    }
+
+    @Override
+    public int compareTo(WeatherReading other) {
+        if (!country.equals(other.country)) {
+            return country.compareTo(other.country);
+        } else if (!state.equals(other.state)) {
+            return state.compareTo(other.state);
+        } else if (!city.equals(other.city)) {
+            return city.compareTo(other.city);
+        } else if (year != other.year) {
+            return year - other.year;
+        } else if (month != other.month) {
+            return month - other.month;
+        } else {
+            return day - other.day;
+        }
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other instanceof WeatherReading) {
+            return this.compareTo((WeatherReading)other) == 0;
+        } else {
+            return false;
+        }
+    }
+}
